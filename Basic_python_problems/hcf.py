e457aff120bfca45611a59f8109b86b689f5ef8f
saimohan <saimohan11sai@gmail.com>
2022-07-05 20:21:25 +0530

    hcf problem new solution using set comprehension

--- Basic_python_problems/hcf.py
+++ Basic_python_problems/hcf.py
@@ -5,3 +5,14 @@ def hcf(x,y):
         return hcf(y,x%y)
 
 print(hcf(12,14))
+
+# define hcf
+# pass two arguements x,y
+# using set comprehension
+def hcf(x,y): 
+    x1 = {i for i in range(1,x+1) if x % i == 0}
+    x2 = {j for j in range(1,y+1) if y % j == 0}
+    y2 = x1.intersection(x2)
+    print(max(y2))
+    
+hcf(54,24)
